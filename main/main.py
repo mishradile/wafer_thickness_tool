5f92aadd68180f286e6619e77f304014c9aa6e95
ChenJiakaiIME <chenjiakaiime@gmail.com>
2022-07-08 11:11:41 +0800

    draft

--- main/main.py
+++ main/main.py
@@ -1,5 +1,6 @@
 import openpyxl 
-from scipy.interpolate import griddata
+from matplotlib.mlab import griddata
+import scipy.interpolate as interp
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -35,13 +36,44 @@ for ws in wb.worksheets:
     
     #Loop through each column
     for col_no in range(var_num):
-        values = df.iloc[:,col_no]
+        z = df.iloc[:,col_no]
+        values = z
 
-        
-        #https://docs.scipy.org/doc/scipy/tutorial/interpolate.html
-        grid_x, grid_y = np.mgrid[min(df.iloc[:,-2]):max(df.iloc[:,-2]):200j, min(df.iloc[:,-1]):max(df.iloc[:,-1]):200j]
-        
-        grid_data= griddata(points, values, (grid_x, grid_y), method='cubic', rescale=True)
+        x = points.iloc[:, 0]
+        y = points.iloc[:, 1]
+        # define grid.
+        xi = np.linspace(-20, 20, 21)
+        yi = np.linspace(-59, 59, 21)
+        # grid the data.
+        zi = griddata(x, y, z, xi, yi, interp='linear')
+
+        #from http:// stackoverflow.com/questions/34489039/ retrieving-data-points-from-scipy-interpolate-griddata
+        xi_coords = {value: index for index, value in enumerate(xi)}
+        yi_coords = {value: index for index, value in enumerate(yi)}
+
+
+        #iterate to find all the griddata z-height values
+        zii = []
+
+        for index, value in enumerate(xi):
+            for index, value2 in enumerate(yi):
+                zii.append(zi[xi_coords[value],yi_coords[value2]])
+
+        #RBF Method
+        xi,yi=np.meshgrid(xi, yi)
+        RBFi = interp.Rbf(xi, yi, zii, function='quintic', smooth=0)
+        # re-grid the data to fit the entire graph
+        xi = np.linspace(-25, 25, 151)
+        yi = np.linspace(-65, 65, 151)
+        xi,yi=np.meshgrid(xi, yi)
+        zi = RBFi(xi, yi)
+
+
+
+        im = plt.figure(num=None, figsize=(9.95, 16.712), dpi=80, facecolor='w', edgecolor='k')
+        # contour the gridded data, plotting dots at the nonuniform data points.
+        CS = plt.contour(xi, yi, zi, 30, linewidths=0.5, colors='k')
+        CS = plt.contourf(xi, yi, zi, 50, cmap=plt.cm.rainbow)
         
         fig, ax = plt.subplots(figsize=(6, 6))
         
@@ -53,7 +85,6 @@ for ws in wb.worksheets:
         plt.plot(points.iloc[:,0], points.iloc[:,1], 'k.', ms=5)
         
         
-        im = plt.imshow(grid_data.T, extent=(min(points.iloc[:,0])-10,max(points.iloc[:,0])+10,min(points.iloc[:,1])-10,max(points.iloc[:,1])+10), origin='lower', cmap = 'jet')
         im.set_clip_path(new_circle)
         #Format colorbar to show values to 2 d.p
         plt.colorbar(format = '%1.0f', shrink =0.75)
